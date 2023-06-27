34195e5baf7efa0e913f641cad45a9ed7f2fe0c6
zrmin <fmrt19zrmin@163.com>
2023-06-27 15:24:09 +0800

    Add opcode macro definition

---
+++ lab2/lc3bsimdefines.h
@@ -0,0 +1,19 @@
+#ifndef __LC3BSIMDEFINES__H
+#define __LC3BSIMDEFINES__H
+
+#define ADD 1
+#define AND 5
+#define BR 0
+#define JMP 12
+#define JSR_R 4
+#define LDB 2
+#define LDW 6
+#define LEA 14
+#define RTI 8
+#define SHF 13
+#define STB 3
+#define STW 7
+#define TRAP 15
+#define XOR 9
+
+#endif

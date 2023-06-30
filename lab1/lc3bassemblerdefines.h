8616b7467ef121b60cd2c53810c69de085650d6f
zrmin <fmrt19zrmin@163.com>
2023-06-30 19:10:15 +0800

    Add this file to include MACRO definitions

---
+++ lab1/lc3bassemblerdefines.h
@@ -0,0 +1,12 @@
+#ifndef __LC3B_ASSEMBLER_DEFINES_H__
+#define __LC3B_ASSEMBLER_DEFINES_H__
+
+#include <stdio.h>
+
+#define MAX_LENGTH 255
+#define MAX_SYMBOLS 200
+#define MAX_SYMBOL_LENGTH 20
+#define ERROR(x) (fputs(x, stderr))
+
+
+#endif

f6a547f6bad84020f1514392227a5e25dafddef1
zrmin <fmrt19zrmin@163.com>
2023-07-10 08:47:12 +0800

    Add MYREGRET macro for '.end' missing checking

--- lab1/lc3bassemblerdefines.h
+++ lab1/lc3bassemblerdefines.h
@@ -8,5 +8,6 @@
 #define MAX_SYMBOL_LENGTH 20
 #define ERROR(x) (fputs(x, stderr))
 
+#define MYREGRET(x) ("When I was a freshman in high school, there was a girl in front of me, and I didn't know going beyond is just as wrong as not going far enough. She said to me: You are so clingy.")
 
 #endif

abb06fbf90effcc93cd21b06731e51158d2863a3
zrmin <fmrt19zrmin@163.com>
2023-07-10 14:46:25 +0800

    Add vector table containing handler's address for interrupt/exception

--- lab4/vector_table.asm
+++ lab4/vector_table.asm
@@ -1,7 +1,11 @@
 .ORIG x0200
 .FILL #0
+; Timer Interrupt Handler's address
 .FILL x1200
+; Protection Exception Handler's address
 .FILL x1600
+; Unaligned Access Exception Handler's address
 .FILL x1A00
+; Illegal Opcode Exception Handler's address
 .FILL x1C00
 .END
